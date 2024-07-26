860d230acb6fb23e3c4cd47d8175ce29b31733cc
Marino Rottier <marino@rottier.nl>
2024-07-26 14:58:43 +0200

    chore: update PDF button

--- src/lib/components/ButtonPDF/ButtonPDF.svelte
+++ src/lib/components/ButtonPDF/ButtonPDF.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-  import { Printer } from "@steeze-ui/heroicons";
+  import { ArrowDownTray } from "@steeze-ui/heroicons";
   import { Icon } from "@steeze-ui/svelte-icon";
   import pdfUrl from '/cv.pdf?url';
 
   function downloadFile(fileName: string) {
-    // const fileUrl = `/${fileName}`;
-
     const anchor = document.createElement("a");
     anchor.href = pdfUrl;
     anchor.download = "Marino Rottier - curriculum vitae.pdf";
@@ -20,5 +18,5 @@
   class="btn btn-outline btn-sm border-1 md:btn-md md:border-2 md:p-2 aspect-square p-1 text-slate-800 opacity-25 hover:opacity-100"
   on:click={() => downloadFile(pdfUrl)}
 >
-  <Icon src={Printer} theme="outline" /></button
+  <Icon src={ArrowDownTray} theme="outline" /></button
 >
